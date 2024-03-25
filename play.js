bcc294401d57e66fc3c675a65e2174fb2c854b93
najia <najiaknizi199@gmail.com>
2024-03-25 11:24:11 +0000

    version final

--- play.js
+++ play.js
@@ -4,8 +4,12 @@ document.addEventListener('DOMContentLoaded', function() {
   let scd = document.getElementById('scd');
 
   // Définir le temps initial à 15 minutes
-  let minutes = 15;
-  let seconds = 0;
+  let minutes = 0;
+<<<<<<< HEAD
+  let seconds = 60;
+=======
+  let seconds = 6;
+>>>>>>> 77a38b43e11366cbe5d31562e151c3248fc5ae31
 
   // Mettre à jour le temps initial sur la page
   min.innerHTML = (minutes < 10 ? "0" : "") + minutes;
@@ -26,6 +30,8 @@ document.addEventListener('DOMContentLoaded', function() {
     // Si le temps est écoulé
     if (minutes === 0 && seconds === 0) {
       clearInterval(timer);
+      showScore();
+      
       // Insérer ici le code pour gérer le temps écoulé
     }
   },1000);
@@ -36,51 +42,162 @@ document.addEventListener('DOMContentLoaded', function() {
 //remplir array 
 const questions=[
 {
-  question:"mon nom est ?",
+  question:"Quelle est la capitale du Japon ?",
     answers:[
-        {text:"najia zen ",correct: false},
-        {text:"najia ghzala",correct:true},
-        {text:"najia khayba",correct:false},
-        {text:"najia sayma",correct:false},
+        {text:"Pékin ",correct: false},
+        {text:"Tokyo",correct:true},
+        {text:"Séoul",correct:false},
+        {text:"Bangkok",correct:false},
 
     ]
 
 },{
-  question:"ilham kidayra ?",
+  question:"Combien de planètes dans notre système solaire?",
     answers:[
-        {text:"ilham zeeen" , correct:true},
-        {text:"ilham hayawanaa",correct:false},
-        {text:"ilham zamra",correct:false},
-        {text:"ilham sa2ima",correct:false},
+        {text:"11" , correct:false},
+        {text:"7",correct:false},
+        {text:"8",correct:true},
+        {text:"10",correct:false},
     ]
 },
 {
-  question:"7na kidayrat ?",
+  question:"Qui a écrit 'Romeo et Juliette'",
+
     answers:[
-        {text:"fenaat" , correct:false},
-        {text:"ghzalat",correct:false},
-        {text:"khaybat",correct:true},
-        {text:"ra2i3at",correct:false},]
+        {text:"William Shakespeare" , correct:true},
+        {text:"Charles Dickens",correct:false},
+        {text:"Jane Austen",correct:false},
+        {text:"Mark Twain",correct:false},]
 },{
     
-        question:"classe kidayra ?",
+        question:"Quel est l'élément le plus abondant dans l'atmosphère terrestre?",
         answers:[
-            {text:"basslin" , correct:true},
-            {text:"5dh",correct:false},
-            {text:"khayba",correct:false},
-            {text:"ra2i3a",correct:false},
+            {text:"Azote" , correct:false},
+            {text:"Hydrogène",correct:false},
+            {text:"Dioxyde de carbone ",correct:false},
+            {text:"Oxygène",correct:true},
         ]
     
 }
+,{
+  question:"What is the opposite of 'hot'?",
+  answers:[
+      {text:"Warm" , correct:false},
+      {text:"Cold",correct:true},
+      {text:"Spicy",correct:false},
+      {text:"Cool",correct:false},
+  ]
+}
+,{
+  question:"Which of the following is a type of fruit?",
+  answers:[
+      {text:"Potato" , correct:false},
+      {text:"Carrot",correct:false},
+      {text:"Apple",correct:true},
+      {text:"Broccoli",correct:false},
+  ]
+}
+,{
+  question:"Complete the famous proverb: 'A bird in the hand is worth...",
+  answers:[
+      {text:"Two in the bush" , correct:false},
+      {text:"One in the tree",correct:false},
+      {text:"None at all",correct:false},
+      {text:"Two in the hand",correct:true},
+  ]
+}
+,{
+  question:"What does the acronym 'NASA' stand for?",
+  answers:[
+      {text:"National Air and Space Association" , correct:false},
+      {text:"National Aeronautics and Space Administration",correct:true},
+      {text:"North American Space Agency",correct:false},
+      {text:"New Astronomical Studies Association",correct:false},
+  ]
+}
+,{
+  question:"Qu'est-ce que HTML?",
+  answers:[
+      {text:"Un langage de programmation pour le développement web" , correct:false},
+      {text:"Un langage de balisage pour créer des pages web",correct:true},
+      {text:"Un langage de programmation pour l'intelligence artificielles",correct:false},
+      {text:"Un système d'exploitation pour les serveurs web",correct:false},
+  ]
+}
+,{
+  question:"Quelle est la signification de CSS ?",
+  answers:[
+      {text:"Cascading Style Sheets " , correct:true},
+      {text:"Computer Software System",correct:false},
+      {text:"UCreative Style System",correct:false},
+      {text:" Coding Style System",correct:false},
+  ]
+}
 
+,{
+  question:"Quel langage de programmation est souvent utilisé pour le développement côté serveur ?",
+  answers:[
+      {text:"HTML " , correct:false},
+      {text:"JavaScript",correct:false},
+      {text:"Python",correct:false},
+      {text:"PHP",correct:true},
+  ]
+}
+,{
+  question:"Quelle est la fonction principale de Git ?",
+  answers:[
+      {text:"Éditer des fichiers texte " , correct:false},
+      {text:"Contrôler les versions de code source et faciliter la collaboration ",correct:true},
+      {text:"Créer des bases de données relationnelles",correct:false},
+      {text:"Analyser des données statistiques",correct:false},
+  ]
+}
+,{
+  question:"Quelle est la signification de l'acronyme 'SQL' ?",
+  answers:[
+      {text:"Structured Query Language " , correct:true},
+      {text:"Simple Question Language",correct:false},
+      {text:"System Quality Language",correct:false},
+      {text:"Server Query Logic",correct:false},
+  ]
+}
+,{
+  question:"Quel est le langage de programmation utilisé pour créer des applications Android ?",
+  answers:[
+      {text:"Java " , correct:true},
+      {text:"Python",correct:false},
+      {text:"C++",correct:false},
+      {text:"JavaScript",correct:false},
+  ]
+}
+,{
+  question:"Qu'est-ce qu'un algorithme ?",
+  answers:[
+      {text:"Un type de serveur web " , correct:false},
+      {text:"Une procédure ou une formule pour résoudre un problème",correct:true},
+      {text:"Un type de base de données",correct:false},
+      {text:"Une fonction JavaScript",correct:false},
+  ]
 
+},{
+  question:"Qu'est-ce que PHP ?",
+  answers:[
+      {text:"Un langage de balisage" , correct:false},
+      {text:"Un langage de programmation pour créer des pages web statiques",correct:false},
+      {text:"Un langage de programmation pour le développement web côté serveu",correct:true},
+      {text:"  Un framework JavaScript",correct:false},
+  ]
+}
 ];
 
 const questionElement=document.getElementById("qst_para");
 const answerBtn = document.getElementsByClassName("choix")[0];
 const nexBtn = document.getElementsByClassName("btn_next")[0];
+const resultat = document.querySelector(".resultat span");
+
 let index=0;
 let scor=0;
+
 function startquiz(){
     index=0;
     scor=0;
@@ -119,6 +236,11 @@ function selectAnswer(e){
   if(isCorrect){
     selectbtn.classList.add("correct");
     scor++;
+<<<<<<< HEAD
+  
+=======
+    localStorage.setItem('scor', scor);
+>>>>>>> 77a38b43e11366cbe5d31562e151c3248fc5ae31
   }else{
     selectbtn.classList.add("incorrect");
   }
@@ -130,11 +252,28 @@ function selectAnswer(e){
   });
   nexBtn.style.display = "block";
 }
+let point=document.getElementById("point");
+const pbar = document.querySelector(".progress");
 function showScore(){
-  resetState();
-  questionElement.innerHTML = `You scord ${scor} out of ${questions.length}!`;
-  nexBtn.innerHTML = "play Again";
-  nexBtn.style.display = "block";
+  resetState(); 
+<<<<<<< HEAD
+  if(scor>=12){
+    questionElement.innerHTML=`Félicitations ! Vous avez gagné ; Vous avez marqué ${scor} sur ${questions.length}.`;
+  }
+  else {
+    questionElement.innerHTML=`Dommage, vous n'avez pas gagné cette fois; Vous avez marqué ${scor} sur ${questions.length}.`;
+  }
+  
+  
+  nexBtn.style.display="none";
+  min.style.display="none";
+  scd.style.display="none";
+  point.style.display="none";
+  pbar.style.display="none";
+
+=======
+window.location.href = '/resultat.html';
+>>>>>>> 77a38b43e11366cbe5d31562e151c3248fc5ae31
 }
 
 
@@ -156,21 +295,44 @@ nexBtn.addEventListener("click",()=>{
 
 });
 
-var prowidth;
-var protext
-function updateProgressBar(progresBar,value){
-  prowidth= progresBar.querySelector(".progresFile").style.width=`${value}%`;
-   protext= progresBar.querySelector(".progresText").textContent=`${value}%`;
-}
-function bar(){
- 
- 
-    const pbar=document.querySelector(".progress");
-    updateProgressBar(pbar,10);
-  
-   
+var prowidth = 0;
 
-  
+function updateProgressBar(progressBar, value) {
+  prowidth = value; // Mettre à jour la valeur de prowidth
+  let roundedValue = Math.round(value); // Arrondir la valeur du pourcentage
+  progressBar.querySelector(".progresFile").style.width = `${roundedValue}%`;
+  progressBar.querySelector(".progresText").textContent = `${roundedValue}%`;
 }
+
+        function bar() {
+            const pbar = document.querySelector(".progress");
+            // Augmenter de 6.25% à chaque clic
+            prowidth += 6.25;
+            // Limiter à 100%
+            prowidth = Math.min(prowidth, 100);
+            updateProgressBar(pbar, prowidth);
+            // Réinitialiser si on atteint 100%
+            if (prowidth >= 100) {
+                prowidth = 0;
+            }
+        }
+
+        
+<<<<<<< HEAD
+    
+=======
+        window.onload=function(){
+          startquiz();
+ 
+          const savescor = localStorage.getItem('scor');
+          if(savescor !== null){
+            scor=parseInt(savescor);
+            resultat.innerHTML = scor;
+           
+          }
+          localStorage.removeItem('scor');
+          
+        }
+>>>>>>> 77a38b43e11366cbe5d31562e151c3248fc5ae31
 
 startquiz();
